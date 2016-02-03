1f5fa89be803e412cc91d73a80ec51a713397aa7
ZStao <zstao1111@foxmail.com>
2016-02-04 00:47:03 +0800

    test

---
+++ src/main/java/com/jee/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.jee.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ZSt on 2016/2/4.
+ */
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body><h1>Hello</h1></body></html>");
+    }
+}
