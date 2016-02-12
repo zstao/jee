b0a954ca1c16234c5b17a6f91592b5940d1cae8e
ZStao <zstao1111@foxmail.com>
2016-02-12 18:08:26 +0800

    db init

--- db/shop/init.sql
+++ db/shop/init.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016-02-06 07:20:16
+-- Generation Time: 2016-02-12 11:07:00
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,16 +23,127 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `address`
+--
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `postcode` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `goods`
+--
+
+CREATE TABLE IF NOT EXISTS `goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `price` double NOT NULL,
+  `intro` text NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `stock` int(10) NOT NULL,
+  `c_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `c_id` (`c_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `goods_orders`
+--
+
+CREATE TABLE IF NOT EXISTS `goods_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `goods_id` int(10) NOT NULL,
+  `orders_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `goods_id` (`goods_id`),
+  KEY `orders_id` (`orders_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `buy_date` datetime NOT NULL,
+  `pay_date` datetime NOT NULL,
+  `confirm_date` datetime NOT NULL,
+  `status` int(5) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`,`address_id`),
+  KEY `address_id` (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nickname` varchar(100) NOT NULL,
+  `type` int(5) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `address`
+--
+ALTER TABLE `address`
+ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- 限制表 `goods`
+--
+ALTER TABLE `goods`
+ADD CONSTRAINT `goods_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `category` (`id`);
+
+--
+-- 限制表 `goods_orders`
+--
+ALTER TABLE `goods_orders`
+ADD CONSTRAINT `goods_orders_ibfk_2` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`),
+ADD CONSTRAINT `goods_orders_ibfk_1` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`);
+
+--
+-- 限制表 `orders`
+--
+ALTER TABLE `orders`
+ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
